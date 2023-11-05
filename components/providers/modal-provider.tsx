06d4a3ba2d5a0676856f4975451921c8190fa608
Sara Öhman <saraohman97@gmail.com>
2023-11-05 10:41:14 +0100

    deploy

---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { SettingsModal } from "@/components/models/settings-modal";
+import { CoverImageModal } from "../models/cover-image-modal";
+
+export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+  
+  return (
+    <>
+      <SettingsModal />
+      <CoverImageModal />
+    </>
+  );
+};
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://usable-eel-94.clerk.accounts.dev",
+            applicationID: "convex",
+        }
+    ]
+}
